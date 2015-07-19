d7f032f7404a6d810c967ff8ced843182287c254
rguichard <rguichard@vsense.fr>
2015-07-19 18:59:24 +0200

    minor change

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'vSense'
 SITENAME = 'vSense'
-SITEURL = ''
+SITEURL = 'http://blog.vsense.fr'
 
 PATH = 'content'
 
@@ -19,10 +19,10 @@ GOOGLE_ANALYTICS = 'UA-33687380-2'
 PLUGIN_PATHS = ['pelican-plugins']
 PLUGINS = ['gravatar','share_post', 'feed_summary']
 
-SUMMARY_MAX_LENGTH = 40
-
 TAGLINE = "Cloud Cloud Cloud, I'm looking for a good time !"
 PROFILE_IMAGE_URL = 'http://i.forbesimg.com/media/lists/companies/google_416x416.jpg'
+
+DISPLAY_PAGES_ON_MENU = True
 
 # Feed generation is usually not desired when developing
 #FEED_ALL_ATOM = None
@@ -32,15 +32,16 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 FEED_USE_SUMMARY = True
 SUMMARY_MAX_LENGTH = 50
+
 # Blogroll
-#LINKS = (('Pelican', 'http://getpelican.com/'),
+#LINKS = (('Pelican', 'http://getpelican.com/'))
 #         ('Python.org', 'http://python.org/'),
 #         ('Jinja2', 'http://jinja.pocoo.org/'),
-#         ('You can modify those links in your config file', '#'),)
+#         ('', '#'),)
 
 # Social widget
 SOCIAL = (('github-square', 'https://github.com/vSense'),
-        ('rss-square', 'http://blog.vsense.fr/feeds/all.atom.xml'))
+        ('rss-square', 'feeds/all.atom.xml'))
 
 DEFAULT_PAGINATION = 10
 
